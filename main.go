d1a69abaf69d9a5209a6c48732f3b0616dcab1b8
Tatiana Johnson <Tatianarjo@gmail.com>
2022-03-30 22:49:41 -0500

    users can update todo status

--- main.go
+++ main.go
@@ -47,6 +47,21 @@ func getTodo(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todo)
 }
 
+func toggleTodoStatus(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+		return
+	}
+
+	todo.Completed = !todo.Completed
+
+	context.IndentedJSON(http.StatusOK, todo)
+
+}
+
 func getTodoById(id string) (*todo, error) {
 	for i, t := range todos {
 		if t.ID == id {
@@ -61,6 +76,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
 	router.GET("/todos/:id", getTodo)
+	router.PATCH("/todos/:id", toggleTodoStatus)
 	router.POST("/todos", addTodos)
 	router.Run("localhost:9090")
 }
